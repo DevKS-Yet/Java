a8130dc16aba93534bf6435ed040f77b3b5d528d
조규상 <@KyusangCho>
2021-05-23 18:26:23 +0900

    Java Basic

---
+++ Java Basic/src/B02_Comments.java
@@ -0,0 +1,7 @@
+
+public class B02_Comments {
+
+	// This is a single-line comment
+	/* This is a multi-line comment */
+	
+}
---
+++ Java Basic/src/B03_VariablesAndDataTypes.java
@@ -0,0 +1,42 @@
+
+public class B03_VariablesAndDataTypes {
+
+	public static void main(String[] args) {
+		
+		//These are 8 primitive data types
+		
+		//byte(1 byte) = -128 ~ 127
+		byte b_min = -128;
+		byte b_max = 127;
+		
+		//short(2 bytes) = -32,768 ~ 32,767;
+		short s_min = -32768;
+		short s_max = 32767;
+		
+		//int(4 bytes) = -2,147,483,648 ~ 2147483647 
+		int i_min = -2147483648;
+		int i_max = 2147483647;
+		
+		//long(8 bytes) = -9,223,372,036,854,775,808 ~ 9223372036854775807
+		long l_min = -9223372036854775808l;
+		long l_max = 9223372036854775807L;
+		
+		//float(4 bytes) = 3.4e-038 ~ 3.4e+038
+		float f_min = 3.4e-038f;
+		float f_max = 3.4e+038F;
+		
+		//double(8 bytes) = 1.7e-308 ~ 1.7e+308;
+		double d_min = 1.7e-308;
+		double d_max = 1.7e+308d;
+		
+		//boolean(1 bit) = true / false
+		boolean b_true = true;
+		boolean b_false = false;
+		
+		//char = https://ko.wikipedia.org/wiki/ASCII
+		char c_0 = 48;
+		char c_A = 65;
+		char c_a = 97;
+		
+	}
+}
