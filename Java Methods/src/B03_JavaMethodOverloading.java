fc82d3bcc2b9dfb94e65150c34eb9f01f987f65f
조규상 <@KyusangCho>
2021-06-04 22:38:39 +0900

    Java Methods

--- Java Methods/src/B03_JavaMethodOverloading.java
+++ Java Methods/src/B03_JavaMethodOverloading.java
@@ -1,21 +1,23 @@
 
 public class B03_JavaMethodOverloading {
-	
-	static int plusMethodInt(int x, int y) {
+
+	static int plusMethod(int x, int y) {
 		return x + y;
 	}
-	
-	static double plusMethodDouble(double x, double y) {
+
+	static double plusMethod(double x, double y) {
 		return x + y;
 	}
 
 	public static void main(String[] args) {
-		
-		int myNum1 = plusMethodInt(8, 5);
-		double myNum2 = plusMethodDouble(4.3, 6.26);
+
+		int myNum1 = plusMethod(8, 5);
+		double myNum2 = plusMethod(4.3, 6.26);
 		System.out.println("int: " + myNum1);
 		System.out.println("double: " + myNum2);
-		
+
 	}
 
 }
+
+// Multiple methods can have the same name as long as the number and/or type of parameters are different.
